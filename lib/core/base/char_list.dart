f1c33c4879480fd1617e4e31626e4a62bdd7fcdd
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-17 15:38:34 +0300

    char list finished

---
+++ lib/core/base/char_list.dart
@@ -0,0 +1,133 @@
+import 'package:ctle/core/base/sliders.dart';
+
+class CharList {
+  final List _charList = [
+    _charList1,
+    _charList2,
+    _charList3,
+    _charList4,
+    _charList5,
+    _charList6,
+    _charList7,
+    _charList8,
+    _charList9,
+    _charList10,
+    _charList11,
+    _charList12,
+    _charList13,
+    _charList14,
+    _charList15,
+    _charList16,
+    _charList17,
+    _charList18,
+    _charList19,
+    _charList20,
+    _charList21,
+    _charList22,
+    _charList23,
+    _charList24,
+    _charList25,
+    _charList26,
+    _charList27,
+    _charList28,
+    _charList29,
+    _charList30,
+    _charList31,
+    _charList32,
+    _charList33,
+    _charList34,
+    _charList35,
+    _charList36,
+    _charList37,
+    _charList38,
+    _charList39,
+    _charList40
+  ];
+  Map<int, String> reSlideCharListAsMap(charListNumber, slideNumber) {
+    return Sliders.reSlide(_charList[charListNumber], slideNumber).split('').asMap();
+  }
+  Map<int, String> slideCharListAsMap(charListNumber, slideNumber) {
+    return Sliders.slide(_charList[charListNumber], slideNumber).split('').asMap();
+  }
+}
+
+const _charList1 =
+    "!6Z>CiM]yŞ5jBV/}RĞGPğ*?JKNn)7oA(vpuİÇÜ:Xb{E2'I3L^W.0Uz,#mx4&ç_ö9DQhgfı-[%+qYlÖ=tOes;adş8üTHrS1kc|<Fw@";
+const _charList2 =
+    ">pŞ+MQN5öqaAbĞFj7Ö;YP%9/mU{DWçe!dR}ül4|gİOx[J-K@o82T*V=^]L0rÜCıvEz6i_cs#'?Ç1yw),&3<S:.GhştBZnğuf(kIHX";
+const _charList3 =
+    "Oo]%PAsWgy9fÇ6b:XjFRMk}[<w@_öZu(n28HVr3E+üNJYC&';S,i=zmaL>ĞÖq#pİU.4ŞTx)*e1D5Gdt!IKh70|Q-/ş^cBvÜçl?ı{ğ";
+const _charList4 =
+    "öX>Rw=tŞ0V:u6hNQf#mlğĞ,KJpL.çDB(+dk@sİgz_Öş[;)8IF4ÜGHCAy^M-YTj]{q9ıen3P2<%x|Ç}bW/a*ZOvü17?'i5&SorcEU!";
+const _charList5 =
+    "üv*ÇW}J[e,ş6G/(IÜKC8#DiğxPZ;gU&b_%F1fmL.pRaötoVÖ-0{OçuıİY]w@2?Ğ)9j:=5lThc^HykNSzq'>A!n<+ErXŞQsB734dM|";
+const _charList6 =
+    "}zŞDötğVmRÜZf9-^nÖ#k3jXşü:8H7T]&<>=_e6Ğ;q+F'i.0vYpCÇQ@2İcyN*alBEK{%?[sdGog4ç,Or5/)uMI(PwUJı|bAShWL1!x";
+const _charList7 =
+    "dx_/ö:ıçİbN>tz?XImğ;WÜkÇi|Ks^9C#]YşP8L+hü<MGgo0}w)5DJS!V{E&7B@4fAraeF1l[*TZv6OŞQp%HRj2'(-U3cquy.,nÖĞ=";
+const _charList8 =
+    "7dFikı[peDyfZĞ>!j(O'<JwN.Q@İLBVÇr3=XSC4AH_0tgbv]+xT5G;ÜI%-^Ö9Mm{n}RlçPo/Uö8suKY|ü:,#ğaŞq?*E&1)ch2zWş6";
+const _charList9 =
+    "Ip.7nİXMGL=bH#wÖ*Ş,-:>@<Ng!x5ztJB6^D]}_{AĞajşQ+K[vTh4ö;E?/Y(eUP%uçÇk'ÜıdsOyVüğ1l)coRm9&|Zifq0WF8r3CS2";
+const _charList10 =
+    "/cu&y491exE=ğM3<8ĞçL!YJ(D6HAÇŞ-ü]*Ö_Ob:5Bpq;#jN7ı'Cş|Fs)@Tn}w.oZR[ÜİX2{gUrGtV0mP^S>hK?zkviW,l%+öIfQad";
+const _charList11 =
+    "z'%-/<Fm:DEYK{ÜB48hq0&çAr6P]iĞ7Ç(})?.^[|25pÖZ!#y1O3VW;RwN=J9CIğfuQclöSo+tUşİvX@ıgŞT_,*LjbdHünxsek>MGa";
+const _charList12 =
+    "y*@>V&[ç=isbuf8,üS]7İöU!_ŞTpL6COR)H0WÇeJv3DxNq^-XwAFQ|hÖ;{?:Kşjno5'ğmPYÜ1kc}ıa.(+gIMtE49ZĞd%z/Gr<2#Bl";
+const _charList13 =
+    ".eq#;F60Ü%1=SAhÇUt@lŞ3İüGg2o,W*<)kPd8şn:{!XxvQÖc}RJsNçbpL&5ĞD+jı|4>ZE9/z_(yOör'KğmaCT-uI^]7?wifV[MHYB";
+const _charList14 =
+    "k,R<^OÜGöJcNIQ1n}#[v+ğKtEL6uPSÖ)7%qs5ç=.wy9/MbgpeC;YıĞ->afT2@roj]?üZ:4d8U*İWX_{'Vş!03BmADxh(Fl&|HÇzŞi";
+const _charList15 =
+    "[ZIU'OÖ<vSgŞNrpAVlw:%(Rs/nı){WM,_>cİ]th+fFĞğÜö4Y^=ia!90XE}?çmJB-ş*yüPKTC@d3L&75xGÇqkzbu18;.D6#Ho|Qj2e";
+const _charList16 =
+    "_GRoEı0,Ç(OXyQx73kNç}=wü8{&-ğg^pU9Ö<?z#%)!WCMZbhYnvAcHqmİöDeÜt'+B[@LKaIfşr.Ts*6/jĞ:Şu2|]lid4VJP;5FS1>";
+const _charList17 =
+    "-bZş;:}O1CgH2>f/)7Ç@{w+PFlq!J8jSmnGN.Vus*%_MX#h5A(LtzaıŞ'İ,o0vYpd]63Bx&yRT|ir^?W[kE<4Dğ=K9ĞöüQÖÜUIeçc";
+const _charList18 =
+    "_(:x?zs-U}Alı<Ğ/bd8ü0nŞK7&Hö#134ğ%^;R5GSPu>Ja!Ei'Yç,2vDorXjşL*İ.ONtZpfw[yF{VW6kÖq@gB)Q]c9ImM=ÇhÜCT|+e";
+const _charList19 =
+    "^{ç#wKy/ÜĞhUI!ğü,t)zm4L%ı1Ç]@S*<Z2WF?jpcT+:gQ|9OC(.ÖöİŞ&->fa8uoNJeVrnAMP5q3;x0s6=şkl_RH}XGb7v'dDEiBY[";
+const _charList20 =
+    "j&{WQ!?İ(DÖLuXt:ğG7c+k%obzşŞfn32v=aM,.xdNFK)Ç|w*>8}0EClg#HTp1üVÜ]YöRO5SçU@e_ı;sZ9iJ^Ih/-<6r[ĞB4PqAmy'";
+const _charList21 =
+    "İCu]ÖXif7/Mdhawek3)R*0VY=!86lc+_rZOü&IÇQjŞb1qF#x(<TmE@vonpzSsK%4;|GJDH^LNW-5[ğ,öAı29}Ğ{y'Üş?gBt>.U:çP";
+const _charList22 =
+    "@r|DcKŞ_bH>T?.,Pw{S3FfÖxC6ZLoG=<Vgvç:kN]&a57YQ%iÇE)j}[(*dÜXIUq;#pehl/2zOA4B0uRşıĞö!n'y^Jİü1mMWs-ğ+98t";
+const _charList23 =
+    "H7rE?c4üçXL<M6':0D}2+5oStzĞöefpsTw-%|!K_/ŞhÖFxBVYW{*8(qNlIıZ;uvİ)@]A,#OiÇm3PR.[J^nğ>kj&91byU=QÜGdCaşg";
+const _charList24 =
+    "m]7}!pŞş<Ö:UoxĞTh@G*vd^Ü0s4-cCflHü1#9O(jı'MQXYS3WJig;öPAİ/.=)5yrZ%[2{na6t>KD+?Ru_e8NB|ğçzVqLI&k,bEÇwF";
+const _charList25 =
+    "*.h6ŞYöuyt?UNİ!3kwCs2dflMçvn[L(rızq}|bğ8oeED{XB]=PĞTgH4ş:%&1x'5pÖ0QcIO-Kü9FÜ7/S><#j+;,JV@)ZmW_ÇR^GAia";
+const _charList26 =
+    "|;dDŞ][yösZİçB=NRj/okJU4+şg'59O?}8Eia-z(l6IvSĞ<f_Ö,1!QÜXn:FGÇWqhHeCL^ğmı7#w{2VY)r.ü3>pMt0b*c&A@Px%KuT";
+const _charList27 =
+    "öa?;(UüZ2b,43}FWu&D!*@h^zÇŞxLvo1SVr7.y#şTECp/%QYk'n]l0R>q8<t|ej)[J+sĞğwBMOÖ-ÜçXm_iNAK:9HIG{=c6gİfıP5d";
+const _charList28 =
+    "RIBY+ğUb4ö(rcpdşngGt3/i5Ş_S8!.0xİ&=h%TQÇ#Hkü-W;VfC[6l^LEK,NPFum>}eM)v{|o:yXw2Üjs7OçaZ<ı'zADÖJ@q]1?*Ğ9";
+const _charList29 =
+    "ZfHqFQhKA>^y[wJv0,ESÖır2kuacTVz%9tUDYi]/d68ÇŞeş<(güs!XG;|ğ)&j4=Nöb+#.o7LplO*PçBİ'@C}mÜ5WĞ-?InMx3R1:{_";
+const _charList30 =
+    "g,fcÖ]NvM}<Tx+rW>qa.j{sK@n7*Ğt:ıeŞE'COP/?Q0ÜD1ÇçZuüLİJ(4#ş63=ARh_ğ-);odUGbVm5&wkyiXö^FH2I|!zpBS%98[lY";
+const _charList31 =
+    "öQjTonK.:+V&[-Uı<I{d03ç|ü(6}c4ğgY]7DES)9ĞlÖh/;bNe1qF'Ş,AÇİ>BvswXrOÜZ*yk_z@=şMf?%!PJ#Cm5tRH8Lpx^iGuaW2";
+const _charList32 =
+    "(a@Zc:b.j8LrüY*%FRPJç=ş}D/7+ö#1{uğ2Ao>|SÜGxd&4kĞ3pİ5?'Kw_q[X,-VnUQÖ9fEiH;)W0gmlMCTe<yvOŞ^!B6stıhÇNI]z";
+const _charList33 =
+    ">Ö-|D(Cö64gJnkŞİ/bl3;ÇcT7jHR5ry9Üq0Qf=mşN'üuzS<tL8Ğ!e1YPsZFOVd?Mç@:oı^%,WKx+{I_p&w)#BA]X}*ğvi[2hGUa.E";
+const _charList34 =
+    "zH5öB:pDIkXZŞJ}L3vf(,U4s-ıG)>/%x+FYbQĞalCRr!cİ;[w|6q@#0NV8gO?Ku.ohn*&]çmÇSd^Ö9_jAüPÜ1Tiye{WE2=M<ğşt'7";
+const _charList35 =
+    "ĞÇpSAsçuTh^q(,%6C|/Q)WJcnBVYF&Üex#!+0ÖR4[zg3PwiIömİ9GüŞZ'-kyı5M7}]O*?XDH>{r<jfb_LKEoşNv2;8ğ1Ut.al@=d:";
+const _charList36 =
+    "QTİWn]mHxÖYK^hG7;f*p)@gBR80çş!{%NUS<köcÇ#Ei_}zP?Ü1JVCO'jqğ4Xwolu>5=rüL3aZ/.DŞI(Mt2|+d&-ı:b[F,y69ĞeAvs";
+const _charList37 =
+    "KkI6;O8hbS3dxPAFNf-r1tsUüvEQnXH<p(Yc>İ%:}aLuo*Mö5RÖJi,gĞ9^0'+ç[@ÇBj7Ü/V4]D?mz|şŞW.e=qıy{&CZ2G#!_)Tlwğ";
+const _charList38 =
+    "Bs>şakzni(]u:İoTbh+L,2FeSqCVtY.W)[c;HARÖ9<OrlP!ğG-I}vy^M&XQ?=_öJü0%Z15dU684gj7Ğ@Ş{K/|ÜEmıp*çD'wN#3Çxf";
+const _charList39 =
+    ")!z}q>ÇodpÖcS_{QHA/7l%OfEr[DwZXVğI.ü@t0sıĞ8öC=gL9j*62u-bJ3hYkm4GM&:y,1RxK#+WŞ'?B;5<Üia^şPN]|TnçUİv(Fe";
+const _charList40 =
+    "TŞ;VDCğüLB/=,hw+a{ıÜc<b?t.eno06)mil&9jWr_%yç5#uEvR:GO4*]fzxFÇ}!8JKPQI@SdUgZ3psXH>^|NÖq(AMöşĞ-['İk1Y27";
