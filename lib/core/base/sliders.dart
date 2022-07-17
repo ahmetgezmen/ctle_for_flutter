a23a133833532b753cd6f92f617d73374111a8b6
Ahmet GEZMEN <ahmet_gezmen_@hotmail.com>
2022-07-17 13:19:59 +0300

    sliders has been edited

--- lib/core/base/sliders.dart
+++ lib/core/base/sliders.dart
@@ -1,5 +1,5 @@
 class Sliders {
-  String slide(String text, int slipNumber) {
+  static String slide(String text, int slipNumber) {
     int len = text.length;
     for (int i = 0; i < slipNumber; i++) {
       text = text[len - 1] + text.substring(0, len - 1);
@@ -7,7 +7,7 @@ class Sliders {
     return text;
   }
 
-  String reSlide(String text, int slipNumber) {
+  static String reSlide(String text, int slipNumber) {
     int len = text.length;
     for (int i = 0; i < slipNumber; i++) {
       text = text.substring(1, len) + text[0];
